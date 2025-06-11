5b621a75fb5dbd473d245132a0ff800ec3144619
tsunekawa <tsunekawa7@gmail.com>
2025-06-11 14:29:08 +0900

    第09回演習デモ

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import Description from './Description'
 function App() {
   const events:TimetableEvent[] = [
     {day: "Wed", period: 2, title: "情報システム設計"},
-    {day: "Wed", period: 3, title: "ITビジネスのフロンティア"}
+    {day: "Wed", period: 3, title: "ITビジネスのフロンティア"},
+    {day: "Wed", period: 4, title: "UX論"}
   ];
+  const username = "中央太郎"
 
   return (
     <>
       <header>
         <h1 className="h-7">Study Planner</h1>
       </header>
-      <Description />
+      <Description username={username} />
       <TimeTable events={events}></TimeTable>
     </>
   );
--- src/Description.tsx
+++ src/Description.tsx
@@ -1,8 +1,9 @@
 
-function Description() {
+function Description({ username }) {
 
     return (
-      <section>
+      <section onClick={()=> alert('クリックしました')} className="bg-sky-500 border border-solid border-black rounded">
+        <p>{username}さん、こんにちは！</p>
         <p><strong>時間割を計画するアプリ</strong>です。<a>詳しく</a></p>
         <p>中央大学生向けです。</p>
       </section>
