6cb6e89a34dd4d60e556ef651b52b998c8942b69
tsunekawa <tsunekawa7@gmail.com>
2025-06-11 14:52:24 +0900

    背景画像のサンプル

--- src/Description.tsx
+++ src/Description.tsx
@@ -2,7 +2,7 @@
 function Description({ username }) {
 
     return (
-      <section onClick={()=> alert('クリックしました')} className="bg-sky-500 border border-solid border-black rounded">
+      <section onClick={()=> alert('クリックしました')} className="bg-[url(/public/find-icon.png)] bg-contain bg-left bg-no-repeat pl-20 border border-solid border-black rounded">
         <p>{username}さん、こんにちは！</p>
         <p><strong>時間割を計画するアプリ</strong>です。<a>詳しく</a></p>
         <p>中央大学生向けです。</p>
